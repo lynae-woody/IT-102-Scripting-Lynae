ef725707a040a187e72fe1055ed87eb99b6d205e
lynae-woody <woody.lynae@student.greenriver.edu>
2025-10-05 22:30:06 -0700

    Added a positive affirmation message

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -4,3 +4,10 @@
 
 #Prompts the user for their name
 name = input("Please enter your name: ")
+
+positive_affirmation = "This is going to be the best quarter!"
+#Print hello name?
+print(name)
+
+#Print a positive affirmation
+print(positive_affirmation)
