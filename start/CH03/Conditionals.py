c93c99fb0d9a9cdd0f3c9d76d068bc768ecfe141
lynae-woody <woody.lynae@student.greenriver.edu>
2025-10-12 22:39:58 -0700

    Asked the user if their day was good or bad

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 # example workign with conditionals
 #By 
+
+#First, I am going to ask the user if their day is good or bad
+question = input("Is your day good? Answer y or n: ")
+
