6b89b34b6bc71f1c5e86a81a3c33e68a546993f2
lynae-woody <woody.lynae@student.greenriver.edu>
2025-10-12 23:30:40 -0700

    Made a loop to print yes it is 10 times

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Lynae Woody
+
+#First, I am going to ask the user if their day is good or bad
+question = input("Is your day good? Answer y or n: ").lower()
+
+#Second, I will analyze the value and will respond accordingly
+if question == 'y':
+    #Creates a loop that prints "Yes it is" 10 times when the user types 'y'
+    number = 1
+    while number < 11:
+        print("Yes it is.")
+        number += 1
+elif question == 'n':
+    print("Aww that's okay! Tomorrow will be a better day.")
+elif question == 'idk':
+    print("That's alright.")
+else:
+    print("Please enter a valid answer of y for yes and n for no.")
