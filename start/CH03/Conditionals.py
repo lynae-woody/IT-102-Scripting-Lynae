8817a80227b820077cf29c49e02fe77ac6cd5cd5
lynae-woody <woody.lynae@student.greenriver.edu>
2025-10-12 22:52:06 -0700

    Practiced first conditional for if user answers y

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Lynae Woody
 
 #First, I am going to ask the user if their day is good or bad
 question = input("Is your day good? Answer y or n: ")
+
+#Second, I will analyze the value and will respond accordingly
+if question == 'y':
+    print("Yes it is.")
 
