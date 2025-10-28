01019df5617741c1fefebe133b78ccdfc1ad483d
lynae-woody <woody.lynae@student.greenriver.edu>
2025-10-27 20:32:04 -0700

    Created script to read hackme.txt file

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Lynae Woody
+
+'''
+A script to open the file and read the contents
+'''
+
+with open("hackme.txt", "r") as test:
+    content = test.read()
+    print("Here is someone to hack - " + "\n" + content)
