e6b64c3d73fce951593d14f30ee5e189767cb4e2
lynae-woody <woody.lynae@student.greenriver.edu>
2025-10-27 21:10:06 -0700

    Created a function called send_message

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -5,16 +5,21 @@
 #First, I am going to ask the user if their day is good or bad
 question = input("Is your day good? Answer y or n: ").lower()
 
-#Second, I will analyze the value and will respond accordingly
-if question == 'y':
+#Create a function called send_message for the "yes it is" loop
+def send_message():
+    if question == 'y':
     #Creates a loop that prints "Yes it is" 10 times when the user types 'y'
-    number = 1
-    while number < 11:
-        print("Yes it is.")
-        number += 1
-elif question == 'n':
-    print("Aww that's okay! Tomorrow will be a better day.")
-elif question == 'idk':
-    print("That's alright.")
-else:
-    print("Please enter a valid answer of y for yes and n for no.")
+        number = 1
+        while number < 11:
+            print("Yes it is.")
+            number += 1
+    #Second, I will analyze the value and will respond accordingly
+    elif question == 'n':
+            print("Aww that's okay! Tomorrow will be a better day.")
+    elif question == 'idk':
+        print("That's alright.")
+    else:
+        print("Please enter a valid answer of y for yes and n for no.")
+
+
+send_message()
