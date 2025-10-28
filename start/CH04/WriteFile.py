11bb1754b5d9c669a6baf3d0c3dc54f650d0fa39
lynae-woody <woody.lynae@student.greenriver.edu>
2025-10-27 19:54:04 -0700

    Made que list, ans list, and loop for questions

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Lynae Woody
+
+'''
+Opens the file hackme.txt and writes the content of the responses into it
+'''
+
+#The list of questions to ask the user!
+
+questions = [
+    "What is your name? ",
+    "What is your favorite color? ",
+    "What was your first pet's name? ",
+    "What is your mother's maiden name? ",
+    "What elementary school did you attend? "
+]
+
+#The list where the answers will be stored
+
+answers = []
+
+#Loop through the questions and save the input to the answers variable
+
+for q in questions:
+    ans = input(q)
+    answers.append({ans})
+print(answers)
